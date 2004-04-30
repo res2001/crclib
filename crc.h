4ae3c90ac5c0b562c01f7f3aa960d813e07c8918
les <devnull@localhost>
2004-04-30 13:28:08 +0000

    Add CRC 8-bit

--- crc.h
+++ crc.h
@@ -4,6 +4,7 @@
 #if defined(LES_CRC)
 /*===============================================================================================*/
 extern unsigned short eval_crc16 (unsigned short crc, const unsigned char *msg, unsigned msg_len);
+extern unsigned char  eval_crc8  (unsigned char crc, const unsigned char *msg, unsigned msg_len);
 /*===============================================================================================*/
 #endif
 
